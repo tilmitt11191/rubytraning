7efd7a100f1cf174a2bc378f7d50a83bd8c2349b
tilmitt11191 <tilmitt11@gmail.com>
2015-11-07 21:14:54 +0900

    PC 2015年 11月  7日 土曜日 21:14:54 JST

--- readcsv.rb
+++ readcsv.rb
@@ -2,14 +2,21 @@
 print "readcsv.rb start\n"
 require "csv" 
 
-#src = "this is readcsv\n"
+#/cygdrive/f/Dropbox/pc/workingtower/home/dropbox/program/ruby/rubytraning
+
 str = "this is" << "\n" + "readcsv" * 2 + "\n"
 print(str, "\n")
 
+CSV.foreach("null-output.csv") do |row|
+	print row
+end
+
+
 #csv_line_read02=[]
-csv_line_read01 = CSV.open("data/null-output.csv")
-puts #(csv_line_read01)
-puts #(csv_line_read01[])
+#csv_line_read01 = []
+#CSV.open("data/null-output.csv")
+#puts #(csv_line_read01)
+#puts #(csv_line_read01[])
 
 #arr = [123, 456, 789]
 #puts csv_line_read01
