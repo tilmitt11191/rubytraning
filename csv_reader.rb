456fdea82daa52d426084e1313ed90f5f09a81e1
tilmitt11191 <tilmitt11@yahoo.co.jp>
2015-11-09 16:06:58 +0900

    macos.local 2015年 11月 9日 月曜日 16時06分58秒 JST

---
+++ csv_reader.rb
@@ -0,0 +1,31 @@
+
+print "readcsv.rb start\n"
+require "csv" 
+#/cygdrive/f/Dropbox/pc/workingtower/home/dropbox/program/ruby/rubytraning
+
+str = "this is" << "\n" + "readcsv" * 2 + "\n"
+print(str, "\n")
+
+#CSV.foreach("null-output.csv") do |row|
+#	print row
+#end
+
+class A
+	def one
+		1
+	end
+end
+
+a = A.new
+p a.one
+
+#csv_line_read02=[]
+#csv_line_read01 = []
+#CSV.open("data/null-output.csv")
+#puts #(csv_line_read01)
+#puts #(csv_line_read01[])
+
+#arr = [123, 456, 789]
+#puts csv_line_read01
+
+print "\nend\n"
---
+++ test.rb
@@ -0,0 +1,2 @@
+print(__ENCODING__);
+print("日本語");
