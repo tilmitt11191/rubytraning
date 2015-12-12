ca0057d2b261ca7e307df3fd61547a1c33dba75a
tilmitt11191 <tilmitt11@yahoo.co.jp>
2015-12-12 15:03:37 +0900

    ubuntu15 2015年 12月 12日 土曜日 15:03:37 JST

--- scraping/html_manager.rb
+++ scraping/html_manager.rb
@@ -2,6 +2,7 @@
 #ruby
 require	"logger"
 require "csv"
+require 'kconv'
 require 'open-uri'
 require 'nokogiri'
 
@@ -13,43 +14,33 @@ class HTML_manager
 	@input_file
 	@cookie
 
-	def initialize(input_url, cookie)
+	attr_reader :input_file
+
+	def initialize(input_url, cookie, charset)
 		@log = Logger.new("log")
+		@log.info("HTML_manager initialize uri[" + input_url + "]")
 		@input_url = input_url
 		@cookie = cookie
-		#@input_row_data = open(@input_url, @cookie).read
-		#@input_row_data = open(THE_URI, { 'Cookie' => cookie_str }) {|f| ... }
 		@input_row_data = open(@input_url, { 'Cookie' => @cookie })
-		@input_file = Nokogiri::HTML.parse(@input_row_data, @input_url)
+		@input_file = Nokogiri::HTML.parse(@input_row_data, @input_url, charset)
+		
+		@log.info("HTML_manager initialize finished")
 	end
 
-	#def read_input_file
-	#def read_html()
-		#@util.info("read_html["+ @input_filename + "]")
-		#if input_filename not exist. fatal
-		#@input_file=Nokogiri::HTML(File.open(@input_filename))
-	#end
-
 	def print_input_file()
-		#@util.info("print input_file "+ input_filename)
+		@log.info("print input_file "+ @input_file.title)
 		print @input_file
 	end
 
 	def get_title()
+		@log.info("get_title "+ @input_file.title)
 		return @input_file.title
 	end
 
-	def output_row_data(output_file_name)
-		#File.open(output_file_name, "w")do |file|
-			#File.open(@input_row_data, "r") do |row|
-			#@input_file.each do |row|
-			#	p row.read
-			#	file << row
-			#end
-		#end
-
-		@input_row_data.each do |line|
-			print "aaa"
+	def output_input_file(output_file_name)
+		@log.info("output_input_file "+ @input_file.title + " to " + output_file_name)
+		File.open(output_file_name, "w") do |file|
+			file.write @input_file
 		end
 	end
 
